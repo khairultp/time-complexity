df4c490383a1d5160bea7e0d726fdd673ad63358
khairul <khairul@khairuls-MacBook-Air.local>
2019-09-15 17:28:35 +0700

    implement load

--- src/main/java/com/khairul/dictionary/MapString.java
+++ src/main/java/com/khairul/dictionary/MapString.java
@@ -1,26 +1,40 @@
 package com.khairul.dictionary;
 
-import java.util.Arrays;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class MapString implements MapService {
 
     @Override
     public String store(Map[] arr) {
-        return Arrays.stream(arr)
-                .map(item -> combinesKeyValue(item))
+        if (arr == null || arr.length == 0)
+            return null;
+
+        return Stream.of(arr)
+                .map(item -> toFlatString(item))
                 .collect(Collectors.joining("\n"));
     }
 
-    private String combinesKeyValue(Map item) {
+    @Override
+    public Map[] load(String text) {
+        if (text == null || text.trim().length() == 0)
+            return null;
+
+        return Stream.of(text.split("\n"))
+                .map(line -> toMap(line))
+                .toArray(Map[]::new);
+    }
+
+    private String toFlatString(Map item) {
         return item.keySet().stream()
                 .map(key -> key + "=" + item.get(key))
                 .collect(Collectors.joining(";")).toString();
     }
 
-    @Override
-    public Map[] load(String text) {
-        return new Map[0];
+    private Map toMap(String line) {
+        return Stream.of(line.split(";"))
+                .map(item -> item.split("="))
+                .collect(Collectors.toMap(kv -> kv[0], kv -> kv[1]));
     }
 }
--- src/test/java/com/khairul/dictionary/MapStringTest.java
+++ src/test/java/com/khairul/dictionary/MapStringTest.java
@@ -1,6 +1,7 @@
 package com.khairul.dictionary;
 
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 import java.util.HashMap;
@@ -27,6 +28,59 @@ public class MapStringTest {
     @Test
     public void Store_Given_AvailableArray_Return_FormattedString() {
         //Arrange
+        Map[] dictionary = dictionary();
+
+        String expected = dictionaryInText();
+        MapService service = new MapString();
+
+        //Act
+        String result = service.store(dictionary);
+
+        //Assert
+        assertNotNull(result);
+        assertTrue(result.contains("="));
+        assertTrue(result.contains(";"));
+        assertTrue(result.contains("\n"));
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void Load_Given_EmptyString_Return_Null() {
+        //Arrange
+        String empty = "";
+        String emptyNull = null;
+
+        MapService service = new MapString();
+
+        //Act
+        Map[] result1 = service.load(empty);
+        Map[] result2 = service.load(emptyNull);
+
+        //Assert
+        assertNull(result1);
+        assertNull(result2);
+    }
+
+    @Test
+    public void Load_Given_AvailableString_Return_ArrayOfMap() {
+        //Arrange
+        String text = dictionaryInText();
+        Map[] expected = dictionary();
+        MapService service = new MapString();
+
+        //Act
+        Map[] result = service.load(text);
+
+        //Assert
+        assertNotNull(result);
+        assertArrayEquals(expected, result);
+    }
+
+    private String dictionaryInText() {
+        return "key1=value1;key2=value2\nkeyA=valueA;keyB=valueB";
+    }
+
+    private Map[] dictionary() {
         Map item1 = new HashMap();
         item1.put("key1","value1");
         item1.put("key2","value2");
@@ -39,17 +93,6 @@ public class MapStringTest {
         dictionary[0] = item1;
         dictionary[1] = item2;
 
-        String expected = "key1=value1;key2=value2\nkeyA=valueA;keyB=valueB";
-        MapService service = new MapString();
-
-        //Act
-        String result = service.store(dictionary);
-
-        //Assert
-        assertNotNull(result);
-        assertTrue(result.contains("="));
-        assertTrue(result.contains(";"));
-        assertTrue(result.contains("\n"));
-        assertEquals(expected, result);
+        return dictionary;
     }
 }
